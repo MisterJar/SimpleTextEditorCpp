9eab1d70d1e8b07b7d9681db12e24ec39e3d98d2
MisterJar <whitehousejared1@gmail.com>
2017-12-01 19:06:50 -0500

    Start of the project
    The start of the project. but this is going to be very small and theres going to be alot of problems so if you do. please tell me.
    I am a loser!

---
+++ simpletext.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+/*
+	MAKE THIS TEXT EDITOR AS SMALL AS POSSIBLE WITH THE FUNCTIONALITY IN MIND
+	THANK YOU VERY MUCH AND MAKE SURE YOU PUT ALOT OF COMMENTS SO POEPLE CAN
+	UNDERSTAND BETTER!
+	Instructions!
+	1. Make the main-function
+	2. Make the program open a empty file.
+	3. Make the program save the file that has been written
+	4. Make the program close and exit.
+	5. Done.
+*/
+// make functions like saving or exiting or something. i dont know
+ofstream file; // initialize the file name orsomething or the function
+int saveFile()
+{
+	// TODO
+}
+int openFile()
+{
+	// TODO
+}
+int exitFile()
+{
+	// TODO
+}
+int main()
+{
+	while (/* condition */) { // while the file is open, do whatever
+		/* code */
+	}
+	if (/* condition */) { // file is saved. exit or something
+		/* code */
+	}
+	return 0;
+}
